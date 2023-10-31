2091604338055edbdddd29b96f8803b46683a70c
4442 <4442@DESKTOP-BT2PDD3>
2023-11-01 00:19:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.Design;
+
+Console.WriteLine("Введите размерность");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+for (int i=0; i<arr.Length; i++)
+{
+    Console.WriteLine($"введите {i + 1} элемент");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int j = 0;
+int M = new int[x];
+for (int i = 0; i < N; i++)
+{
+    if (arr[i] = 2)
+    {
+        M[j] = i;
+        j++;
+    }
+    else
+    { 
+
+    }
+}
+Console.WriteLine(arr[i]);
